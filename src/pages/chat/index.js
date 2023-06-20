f012a953cf01c88cadeb35e562900a41753abbef
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2023-06-20 12:59:00 +0500

    table

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -11,6 +11,14 @@ import {
   SwipeableDrawer,
   useMediaQuery,
   useTheme,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Alert,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import DehazeIcon from "@mui/icons-material/Dehaze";
@@ -18,9 +26,41 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import FiberManualRecordOutlinedIcon from "@mui/icons-material/FiberManualRecordOutlined";
 import AddIcon from "@mui/icons-material/Add";
+import ErrorIcon from "@mui/icons-material/Error";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import CheckIcon from "@mui/icons-material/Check";
 import SendText from "./sendText";
 import AlertDialog from "./alertDialog";
+
+const sampleData = [
+  {
+    id: "477",
+    name: "Abbott - Pacocha",
+    industry: "IT",
+    city: "Colorado Springs",
+    state: "CO",
+    segment: "Enterpise",
+    ownerId: 7,
+  },
+  {
+    id: "478",
+    name: "Abbott - Pacocha",
+    industry: "IT",
+    city: "Colorado Springs",
+    state: "CO",
+    segment: "Enterpise",
+    ownerId: 7,
+  },
+  {
+    id: "479",
+    name: "Abbott - Pacocha",
+    industry: "IT",
+    city: "Colorado Springs",
+    state: "CO",
+    segment: "Enterpise",
+    ownerId: 7,
+  },
+];
 export default function Index() {
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
@@ -30,6 +70,8 @@ export default function Index() {
 
   const [showOpenChats, setShowOpenChats] = useState(false);
   const [showRecentClosedChats, setShowRecentClosedChats] = useState(false);
+  const [chatAbout, setShowChatAbout] = useState(false);
+
   const [showAlert, setShowAlert] = useState(true);
 
   useEffect(() => {
@@ -97,6 +139,7 @@ export default function Index() {
           <Grid
             container
             justifyContent='space-between'
+            wrap='nowrap'
             sx={{ cursor: "pointer" }}
             onClick={() => setShowOpenChats((c) => !c)}
           >
@@ -127,6 +170,7 @@ export default function Index() {
           <Grid
             container
             justifyContent='space-between'
+            wrap='nowrap'
             sx={{ cursor: "pointer" }}
             onClick={() => setShowRecentClosedChats((c) => !c)}
           >
@@ -184,7 +228,9 @@ export default function Index() {
                       background: "#A9CBFF",
                     },
                   }}
-                  onClick={() => {}}
+                  onClick={() => {
+                    setShowChatAbout(true);
+                  }}
                 >
                   List my top 10 accounts
                 </Button>
@@ -508,6 +554,165 @@ export default function Index() {
       </Grid>
     </Grid>
   );
+
+  const px = { md: "0px 32px 0px 89px", xs: "0px 30px" };
+
+  const tableHeadSx = {
+    background: "#A9CBFF",
+  };
+  const topTenUsers = (
+    <Grid
+      container
+      gap='22px'
+      sx={{
+        height: "100%",
+        mb: "50px",
+        mt: "25px",
+      }}
+    >
+      {/* logo */}
+      <Grid item>
+        <img src='/dev/favicon.png' />
+      </Grid>
+      <Grid item sx={{ flex: 1 }}>
+        <Typography variant='subtitle1' fontSize='20px' sx={{ mt: "10px" }}>
+          To find out how many accounts you have, you can use the following
+          query
+        </Typography>
+
+        <TableContainer
+          component={Paper}
+          sx={{
+            mt: "10px",
+            borderRadius: "10px",
+            maxHeight: "340px",
+            overflowY: "auto",
+          }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+            <TableHead sx={tableHeadSx}>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Industry</TableCell>
+                <TableCell>City</TableCell>
+                <TableCell>State</TableCell>
+                <TableCell>Segment</TableCell>
+                <TableCell>Owner ID</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sampleData.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {row.id}
+                  </TableCell>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.industry}</TableCell>
+                  <TableCell>{row.city}</TableCell>
+                  <TableCell>{row.state}</TableCell>
+                  <TableCell>{row.segment}</TableCell>
+                  <TableCell>{row.ownerId}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* view or download CSV */}
+        <Grid container gap='10px' alignItems='center' sx={{ mt: "20px" }}>
+          <Grid item>
+            <ErrorIcon />
+          </Grid>
+          <Grid item>
+            <Typography variant='body2'>
+              Only 1000 rows are shown,{" "}
+              <span style={{ color: "#213BC2" }}>Click here</span> to view or
+              download CSV
+            </Typography>
+          </Grid>
+        </Grid>
+        <Alert
+          sx={{ mt: "25px" }}
+          severity='error'
+          action={
+            <Button
+              color='inherit'
+              size='small'
+              sx={{
+                color: "#fff",
+                background: "#F0671A",
+                padding: "10px 20px",
+                textTransform: "none",
+              }}
+            >
+              Human Help
+            </Button>
+          }
+        >
+          We are not confident in this answer, but recommended verification
+        </Alert>
+      </Grid>
+    </Grid>
+  );
+
+  const userMenu = (
+    <Grid container alignItems='center' sx={{ gap: "10px" }}>
+      {/* user menu */}
+      <Grid item>
+        {settingMenu}
+        <Grid
+          container
+          alignItems='center'
+          gap='10px'
+          onClick={handleOpenUserMenu}
+          sx={{ p: 0, cursor: "pointer" }}
+          aria-owns={anchorElUser ? profileMenuID : undefined}
+          aria-haspopup={anchorElUser ? true : false}
+        >
+          <Grid item>
+            <Avatar
+              alt='Name'
+              src='/dev/user.svg'
+              style={{ width: "36px", height: "36px" }}
+            />
+          </Grid>
+          <Grid
+            item
+            sx={{
+              display: { md: "initial", xs: "none" },
+            }}
+          >
+            <Typography variant='subtitle2'>Alexey</Typography>
+          </Grid>
+
+          <KeyboardArrowDownIcon
+            sx={{
+              display: { md: "initial", xs: "none" },
+            }}
+          />
+        </Grid>
+      </Grid>
+      {/* toggler */}
+      <Grid item>
+        <IconButton
+          aria-label='show more'
+          aria-haspopup='true'
+          onClick={() => setShowSidebar(true)}
+          color='inherit'
+          sx={{
+            display: { xs: "initial", md: "none" },
+            padding: 0,
+          }}
+        >
+          <DehazeIcon size='large' color='primary' />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+
   return (
     <Grid container wrap='nowrap' sx={{ minHeight: "100vh" }}>
       {sidebar}
@@ -527,96 +732,136 @@ export default function Index() {
           flex: 1,
         }}
       >
-        <Grid
-          container
-          direction={"column"}
-          sx={{
-            padding: { md: "32px 32px 40px 89px", xs: "22px 30px" },
-            height: "100%",
-          }}
-        >
-          {/* header */}
-          <Grid item sx={{ width: "100%" }}>
-            <Grid
-              container
-              justifyContent={{ xs: "space-between", md: "flex-end" }}
-            >
-              {/* logo */}
-              <Grid item sx={{ display: { xs: "initial", md: "none" } }}>
-                <img
-                  src='/dev/logo.svg'
-                  style={{ width: "180px", height: "47px" }}
-                />
+        {chatAbout ? (
+          <Grid
+            container
+            direction={"column"}
+            sx={{
+              padding: { md: "32px 0px 40px 0px", xs: "22px 0px" },
+              height: "100%",
+            }}
+          >
+            {/* header */}
+            <Grid item sx={{ width: "100%", p: px }}>
+              <Grid container gap='15px' justifyContent='space-between'>
+                <Grid item sx={{ flex: 1 }}>
+                  <Grid container wrap='nowrap' gap='31px' alignItems='center'>
+                    <img src='/dev/comment_black.png' />
+                    <Typography
+                      variant='subtitle1'
+                      fontWeight='500'
+                      sx={{ color: "#000" }}
+                    >
+                      Chat about List my top 10 accounts
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>{userMenu} </Grid>
               </Grid>
-              {/* user */}
-              <Grid item>
-                <Grid container alignItems='center' sx={{ gap: "10px" }}>
-                  {/* user menu */}
-                  <Grid item>
-                    {settingMenu}
-                    <Grid
-                      container
-                      alignItems='center'
-                      gap='10px'
-                      onClick={handleOpenUserMenu}
-                      sx={{ p: 0, cursor: "pointer" }}
-                      aria-owns={anchorElUser ? profileMenuID : undefined}
-                      aria-haspopup={anchorElUser ? true : false}
-                    >
-                      <Grid item>
-                        <Avatar
-                          alt='Name'
-                          src='/dev/user.svg'
-                          style={{ width: "36px", height: "36px" }}
-                        />
-                      </Grid>
-                      <Grid
-                        item
-                        sx={{
-                          display: { md: "initial", xs: "none" },
-                        }}
-                      >
-                        <Typography variant='subtitle2'>Alexey</Typography>
-                      </Grid>
-
-                      <KeyboardArrowDownIcon
-                        sx={{
-                          display: { md: "initial", xs: "none" },
-                        }}
-                      />
-                    </Grid>
-                  </Grid>
-                  {/* toggler */}
-                  <Grid item>
-                    <IconButton
-                      aria-label='show more'
-                      aria-haspopup='true'
-                      onClick={() => setShowSidebar(true)}
-                      color='inherit'
-                      sx={{
-                        display: { xs: "initial", md: "none" },
-                        padding: 0,
-                      }}
-                    >
-                      <DehazeIcon size='large' color='primary' />
-                    </IconButton>
-                  </Grid>
+            </Grid>
+            {/* divider */}
+            <Grid
+              item
+              sx={{ width: "100%", borderTop: "1px solid #DDD7D7", mt: "40px" }}
+            />
+            {/* avatar */}
+            <Grid item sx={{ width: "100%", p: px, mt: "19px" }}>
+              <Grid container alignItems='center' gap='24px'>
+                <Grid item>
+                  <Avatar
+                    alt='Name'
+                    src='/dev/user.svg'
+                    style={{ width: "46px", height: "46px" }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant='subtitle1' fontSize='20px'>
+                    List my top 10 accounts
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* center */}
+            <Grid
+              item
+              sx={{
+                width: "100%",
+                flex: 1,
+                mt: { md: "20px", xs: "40px" },
+                p: px,
+                background: "#F0F0F0",
+              }}
+            >
+              {topTenUsers}
+            </Grid>
+            {/* input */}
+            <Grid item sx={{ mt: "20px", p: px }}>
+              <Grid container gap={"15px"}>
+                <Grid item sx={{ flex: 1 }}>
+                  <SendText onSuccess={() => {}} />
+                </Grid>
+                <Grid item sx={{ display: "flex" }}>
+                  <Button
+                    color='inherit'
+                    startIcon={<CheckIcon />}
+                    sx={{
+                      height: "100%",
+                      color: "#fff",
+                      background: "#1ADB73",
+                      padding: "10px 20px",
+                      textTransform: "none",
+                    }}
+                  >
+                    End Chat
+                  </Button>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          {/* center */}
+        ) : (
           <Grid
-            item
-            sx={{ width: "100%", flex: 1, mt: { md: "10px", xs: "40px" } }}
+            container
+            direction={"column"}
+            sx={{
+              padding: { md: "32px 0px 40px 0px", xs: "22px 0px" },
+              height: "100%",
+            }}
           >
-            {exampleActions}
+            {/* header */}
+            <Grid item sx={{ width: "100%", p: px }}>
+              <Grid
+                container
+                justifyContent={{ xs: "space-between", md: "flex-end" }}
+              >
+                {/* logo */}
+                <Grid item sx={{ display: { xs: "initial", md: "none" } }}>
+                  <img
+                    src='/dev/logo.svg'
+                    style={{ width: "180px", height: "47px" }}
+                  />
+                </Grid>
+                {/* user */}
+                <Grid item>{userMenu}</Grid>
+              </Grid>
+            </Grid>
+            {/* center */}
+            <Grid
+              item
+              sx={{
+                width: "100%",
+                flex: 1,
+                mt: { md: "10px", xs: "40px" },
+                p: px,
+              }}
+            >
+              {exampleActions}
+            </Grid>
+            {/* input */}
+            <Grid item sx={{ mt: "20px", p: px }}>
+              <SendText onSuccess={() => {}} />
+            </Grid>
           </Grid>
-          {/* input */}
-          <Grid item sx={{ mt: "20px" }}>
-            <SendText onSuccess={() => {}} />
-          </Grid>
-        </Grid>
+        )}
       </Grid>
     </Grid>
   );
